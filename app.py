3c5fd51ca0868ff4375c1367108e22d48b84d1b7
CarlesGllg <cgallego@robotix.es>
2025-04-22 17:47:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google.auth.transport.requests import Request
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
